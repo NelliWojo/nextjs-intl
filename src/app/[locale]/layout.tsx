338f053d3fdeea9b81173da5543782b3ddb30980
NelliWojo <nelli.timorshina@wojo.com>
2023-12-29 11:54:04 +0100

    feat :wrench: set up intl config

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { useLocale } from 'next-intl'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,13 +10,14 @@ export const metadata: Metadata = {
   description: 'Made with love by Nelli',
 }
 
-export default function RootLayout({
+export default function LocaleLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const locale = useLocale()
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>{children}</body>
     </html>
   )
